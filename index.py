b7284dd5fc3601b8ee2707e26fe9df87d04d0e20
Sivasankar <websivasankar@gmail.com>
2026-03-18 21:44:15 +0530

    all files 18

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import shutil
 # PATHS
 # ==============================
 
-source_folder = r"D:\PositionalSystem\screener\output\2026-03-17"
+source_folder = r"D:\PositionalSystem\screener\output\2026-03-18"
 dest_folder = r"D:\PositionalSystem\screener\gitpublic"
 
 # analysis pages
